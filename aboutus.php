3f1e131a01f43747853f842296cc54e959b6aa1b
Alok Behera <kumaralok.odisha@gmail.com>
2020-06-22 13:22:05 +0530

    my first initial commit

---
+++ aboutus.php
@@ -0,0 +1,186 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    <ul>
+      <li class="placeholder" style="background-image:url(images/demo/image1.jpg);">Image Holder</li>
+      <li><a class="swap" style="background-image:url(images/demo/img3.jpg);" href="#gallery"><strong>Services</strong><span><img src="images/demo/image1.jpg" alt="" /></span></a></li>
+      <li><a class="swap" style="background-image:url(images/demo/img1.jpg);" href="#gallery"><strong>Products</strong><span><img src="images/demo/shr_290913_hospital2.jpg" alt="" /></span></a></li>
+      <li class="last"><a class="swap" style="background-image:url(images/demo/img2.jpg);" href="#gallery"><strong>Company</strong><span><img src="images/demo/shr_290913_hospital5.jpg" alt="" /></span></a></li>
+    </ul>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+      <h1>About Sparsh Hospital</h1>
+      <p><strong>Introduction:</strong><br/>
+      <p align="justify">Sparsh Hospital & Critical care is a 100 Bedded multi-specialty state-of-the-art Hospital in Odisha. It provides comprehensive Healthcare India services. It is the only hospital in the private sector that has maintained nearly 100% bed occupancy due to its reputation of providing the highest level of medical services to patients from Odisha and neighboring states.The hospital started functioning in 2nd Feb 2007 , it boasts of 100 beds with one of the largest Intensive Care Units (ICU), most advanced 2 Operation Theaters, more than 50 consultants and manpower of nearly 400.Hospital attends to around 30 In-patients and 300 Out-patients daily. The hospital is located in the heart of Bhubaneswar and is very close to the international airport. It was established in Sahidnagar on a plot of land approximately 30000 sq.ft. Sparsh Hospital & Critical care continues to maintain its charitable character in accordance with to the wishes of its founder. Funds generated from the hospital services are partially utilised for providing free health care to the poor and needy patients.  </p>
+
+      <p style="background-color:green; padding:5px; color:#fff;"><strong><span style="font-size: medium;">Mission</span></strong></p>
+      <strong><span style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">We are committed to :</span></strong>
+        <ul type="disc" style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Setting the standard for excellence in the delivery of patient care, patient safety, and the quality of the healthcare experience.</li>
+            </ul>
+              <ul type="disc" style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Providing exemplary clinical setting for educating the healthcare delivery professionals who will form the collaborative healthcare delivery tear of tomorrow.</li>
+            </ul>
+             <ul type="disc" style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Leading in the introduction of innovative methodologies for healthcare delivery and quality improvement.</li>
+            </ul>
+             <ul type="disc" style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Provide comprehensive, caring &amp; cost effective medical treatment of global standard, through a team of highly qualified &amp; committed medical professionals with state of the art medical equipment.</li>
+            </ul>
+             <ul type="disc" style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Fulfill of social responsibility to the underprivileged by providing free or subsidized medical treatment.</li>
+            </ul>
+
+            <p style="background-color:green; padding:5px; color:#fff;"><strong><span style="font-size: medium;">Vision</span></strong></p>
+            <span style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">To create an enduring legacy in medical care and well-being using state-of-the art technology and processes that stand for the ultimate in care.</span>
+            <span style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">To differentiate ourselves among our competitors as the most patient friendly and easiest healthcare institution to navigate, meaning :</span
+               <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Customer service</li>
+            </ul>
+              <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Quality</li>
+            </ul>
+              <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Compassion</li>
+            </ul>
+             <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Excellence</li>
+            </ul>
+             <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Safety</li>
+            </ul>
+              <ul style="color: rgb(64, 64, 64); font-family: Arial, Helvetica, sans-serif; line-height: 20px;">
+                <li>Efficiency</li>
+            </ul>
+      <div class="homecontent">
+        <ul>
+          <li>
+           <h2>Super Specialty Hospital</h2>
+            <p class="imgholder"><img src="images/spciality.jpg" alt="" style="width:286px;height:100px;"  /></p>
+          </li>
+          <li class="last">
+            <h2>24X7 services</h2>
+            <p class="imgholder"><img src="images/24x7.png" alt="" style="width:286px;height:100px;"   /></p>
+          </li>
+        </ul>
+        <div class="clear"></div>
+      </div>
+      <p><strong>Directors message:</strong><br />
+    
+        <tr>
+            <td width="14%" bordercolor="0">
+            <div style="text-align: left;"><img src="http://www.sparshhospitals.com/maple/userfiles/image/Dr_Priyabrata%20Dhir_3.jpg" width="200" height="192" alt=""></div>
+            </td>
+            <td width="86%" valign="top">
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in">&nbsp;<span style="font-size: medium;"><strong>Dr. Priyabrata Dhir</strong></span><o:p></o:p></p>
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><strong><span style="font-stretch: normal; font-size: 7pt;">&nbsp;</span>(Chairman-cum- Managing Director)</strong><br>
+            <o:p></o:p></p>
+            <p style="text-align: justify;"><span style="text-align: justify;">Dr. Priyabrata Dhir a general physician by profession has initiated the movement after a successful tenure as an Assistant &nbsp;Surgeon at BSR Bhilai Cancer Institute for more than three years . Dr. Priyabrata Dhir is the Chairman-cum-Managing Director &nbsp;of the Company. He had completed his MBBS from College of Medical Science, Kathmandu University, Nepal in 2002 &nbsp; &nbsp; &nbsp;including one year rotator internship from SCB Medical College, Cuttack. He has garnered adequate experience &amp; exposure in &nbsp;clinical field by serving in different capacities in NTPC General Hospital, BSR Cancer Hospitals &amp; Research Institute, Bhilai &nbsp;and Hemalata Hospital, Bhubaneswar. His skill &amp; exposure shaped him as a specialist in Onco surgery &amp; treatment. He had &nbsp;the dream to have his own hospital &amp; the same was fructified in 2007 in shape of Sparsh. He is the pivotal of Sparsh Hospital &amp; &nbsp;has been stewarding the Sparsh Brand to the nook &amp; corner of the State. He is a great leader &amp; a well known social worker &nbsp;who is being duly epitomized in his contribution to the society in the field of health.&nbsp;</span></p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+            <p align="justify">&nbsp;</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <div align="left"><img src="http://sparshhospitals.com/maple/userfiles/image/2.png" alt=""></div>
+            </td>
+            <td valign="top">
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><span style="font-size: medium;"><strong>&nbsp;Mr. Debabrata Dhir</strong></span><o:p></o:p></p>
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><strong>&nbsp;(Director)</strong><o:p></o:p></p>
+            <p>&nbsp;Sri Debabrata Dhir a Post-Graduate and a Social activist. He is a popular social worker and known for &nbsp;his fanatic spirit to disseminate philanthropic &amp; spiritual ideology which is amply visible in his endeavor to spread spiritualism.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: justify;">&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+            <div align="left"><img src="http://sparshhospitals.com/maple/userfiles/image/3.png" width="144" height="176" alt=""></div>
+            </td>
+            <td valign="top">
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><span style="font-size: medium;"><strong>&nbsp;Er. Satyabrata Dhir</strong></span><o:p></o:p></p>
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><strong>&nbsp;(Director)</strong><o:p></o:p></p>
+            <p align="justify">&nbsp;Er. Satyabrata Dhir a Civil Engineer by profession. He is into the civil construction and industrial business since the completion &nbsp;of his academics about ten years back. He has earned distinctive &nbsp;recognition as a Real Estate Entreprenurer. His acumen as a businessman is unparallel outstanding. He is blessed with modest temperament that &nbsp;contributes to strengthen his qualities to sustain against all odds.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: justify;">&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+            <div align="left"><img src="http://sparshhospitals.com/maple/userfiles/image/4.png" width="144" height="176" alt=""></div>
+            </td>
+            <td valign="top">
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><span style="font-size: medium;"><strong>&nbsp;Dr. Ghanashyanm Biswas</strong></span><o:p></o:p></p>
+            <p class="MsoNormal" style="margin-top:1.0pt;margin-right:0in;margin-bottom:1.0pt;
+            margin-left:.25in;text-indent:-.25in;mso-list:l0 level4 lfo1;tab-stops:list .25in"><strong>&nbsp;(Director)</strong><o:p></o:p></p>
+            <p align="justify">&nbsp;Dr. Ghanashyam Biswas an eminent Medical Oncologist of the state and prominent figure in the cancer society of the country. He is the leading Medical Oncologist of Eastern India. He is a well known name in the field of &nbsp;Cancer Treatment in Odisha &amp; outside Odisha. He had served as a medical oncologist in TATA Memorial Hospital, Mumbai. &nbsp;His association with TMH contributes ample exposure to his professional expertise. He has published numbers of research &nbsp;papers in medical oncology which add feathers to his professional identity.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" style="text-align: justify;">&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+            <div align="left"><img src="http://sparshhospitals.com/maple/userfiles/image/5.png" width="144" height="176" alt=""></div>
+            </td>
+            <td valign="top"><strong>&nbsp;<span style="font-size: medium;">CA. B. Nayak</span><br>
+            &nbsp;(Director Finance)</strong><br>
+            <p align="justify">CA. B. Nayak, had been inducted in the Board of the company as Director, Finance. The Induction of a Chartered Accountant in the Board is aimed at demonstrating high quality Corporate Governance. He has been entrusted with the task of looking after Finance, Accounts, Legal Compliance to ensure high quality financial discipline. Mr. Nayak has 16 years’ post qualification experience as a Chartered Accountant &amp; entails heap of exposure under his cap to drive the company with professional impetus.</p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">&nbsp;</td>
+        </tr>
+        <tr>
+            <td>
+            <div align="left">&nbsp;</div>
+            </td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="2">&nbsp;</td>
+        </tr>
+    </p>
+    </div>
+    <div id="column">
+      <div id="featured">
+        <ul>
+          <li>
+            <h2>Our Services</h2>
+            <p class="imgholder"><img src="images/SDM-Image-5.png" alt="" style="width:240px;height:90px;" /></p>
+          </li>
+          <li role="menuitem">S D M Super Specialty Hospital</li>
+          <li aria-haspopup="Sidebar1_Menu2:submenu:36" role="menuitem">Diagnostic Services</li>
+          <li role="menuitem">Consultation</li>
+          <li role="menuitem">In Patient Services</li>
+          <li role="menuitem">Health Checkup Packages</li>
+          <li role="menuitem">24 X 7 Services</li>
+          <li role="menuitem">Physiotherapy Services</li>
+          <li role="menuitem"><a href=""  tabindex="-1">Dialysis</a></li>
+          <li role="menuitem"><a href=""  tabindex="-1">Insurance Schemes</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
